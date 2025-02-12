a86220a80adb29cee118d264ced6d2607db8299e
Your Name <ksenia-gromova@mail.ru>
2025-02-12 09:46:37 -0700

    authentication

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+import logo from '../assets/logo.png';
+
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logo} alt="Proshop"></img> ProShop
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart /> Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg="success" style={{ marginLeft: '5px' }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link>
+
+              <Nav.Link as={Link} to="/signin">
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
